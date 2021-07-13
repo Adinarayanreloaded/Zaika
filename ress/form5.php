715d40b6749572c89f1a5b9e066f168dc95f3058
Aditya Narayan Sharma <37697073+Adinarayanreloaded@users.noreply.github.com>
2021-07-13 22:42:32 +0530

    Add files via upload

---
+++ ress/form5.php
@@ -0,0 +1,48 @@
+<?php include 'sendemail3.php'; ?>
+<?Php
+
+$insert = false;
+if(isset($_POST['Name'])){
+    // Set connection variables
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+
+    // Create a database connection
+    $con = mysqli_connect($server, $username, $password);
+
+    // Check for connection success
+    if(!$con){
+        die("connection to this database failed due to" . mysqli_connect_error());
+    }
+    // echo "Success connecting to the db";
+
+    // Collect post variables
+    $Name = $_POST['Name'];
+    $Email = $_POST['Email'];
+    $phone = $_POST['phone'];
+    $city = $_POST['city'];
+    $Date = $_POST['Date'];
+    // $phone = $_POST['phone'];
+    // $city = $_POST['city'];
+    $sql = "INSERT INTO `service`.`service` (`Name`,`Email`,`phone`,`city`,`Date`) VALUES ('$Name', '$Email','$phone','$city','$Date');";
+    echo $sql;
+   
+        if($con->query($sql) == true){
+            // echo "Successfully inserted";
+    
+            // Flag for successful insertion
+            $insert = true;
+        }
+        else{
+            echo "ERROR: $sql <br> $con->error";
+        }
+
+        header("location:thankyou.html");
+        
+        $con->close();
+    
+}
+?>
+<!-- , `phone`, `city`, -->
+<!-- , '$phone', '$city' -->
